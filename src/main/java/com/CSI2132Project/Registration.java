8b3c3e1da0336d233dd65ea879b3d57f5fa3429d
Pradyu <90668092+pradyuv@users.noreply.github.com>
2024-03-13 11:38:24 -0400

    addCustomer method implemented but not tested. Login.java and Registration.java made into child classes of UserManagement.java for abstraction.

---
+++ src/main/java/com/CSI2132Project/Registration.java
@@ -0,0 +1,14 @@
+package com.CSI2132Project;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class Registration extends UserManagement{
+     /*
+    The functionality for this class will be similar to login, in that it will check if a user is already registered. If they are, they will not be allowed
+    to register again. If they are not, they will be allowed to register and it will setup a new user in the database.
+     */
+
+}
