899327823f1f36c090bb5b6fe26e50773b4fcbb5
Pradyu <pvasu009@uottawa.ca>
2024-03-19 12:49:49 -0400

    Create Employee.java

---
+++ src/main/java/com/CSI2132Project/Employee.java
@@ -0,0 +1,93 @@
+package com.CSI2132Project;
+
+public class Employee {
+    private int employeeId;
+    private String streetName;
+    private int streetNum;
+    private String postalCode;
+    private String name;
+    private String phone;
+    private String position;
+    private String SIN;
+
+    // Constructor
+    public Employee(int employeeId, String streetName, int streetNum, String postalCode,
+                    String name, String phone, String position, String SIN) {
+        this.employeeId = employeeId;
+        this.streetName = streetName;
+        this.streetNum = streetNum;
+        this.postalCode = postalCode;
+        this.name = name;
+        this.phone = phone;
+        this.position = position;
+        this.SIN = SIN;
+    }
+
+    // Getters
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public int getStreetNum() {
+        return streetNum;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getSIN() {
+        return SIN;
+    }
+
+    // Setters
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public void setStreetNum(int streetNum) {
+        this.streetNum = streetNum;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSIN(String SIN) {
+        this.SIN = SIN;
+    }
+
+
+}
